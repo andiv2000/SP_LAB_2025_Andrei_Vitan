40d9f6945e8c9857269339c1e29cd010afc424ef
unknown <Andrei Vitan@DESKTOP-9NS8UQD.e-uvt.ro>
2025-10-16 13:28:30 +0300

    Initial commit

---
+++ src/main/java/ro/uvt/info/desingpatternslab2025/DesingPatternsLab2025Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.desingpatternslab2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesingPatternsLab2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesingPatternsLab2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/desingpatternslab2025/DesingPatternsLab2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.desingpatternslab2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesingPatternsLab2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
