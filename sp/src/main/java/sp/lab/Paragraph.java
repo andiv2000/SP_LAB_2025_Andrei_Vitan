96ec79ad07e7d880d55cb7d68c0c1c9f4b9a6f5d
unknown <Andrei Vitan@DESKTOP-9NS8UQD.e-uvt.ro>
2025-11-26 21:07:47 +0200

    Lab 2: Implement Strategy pattern for paragraph alignment (AlignLeft, AlignCenter, AlignRight, AlignJustify)

--- sp/src/main/java/sp/lab/Paragraph.java
+++ sp/src/main/java/sp/lab/Paragraph.java
@@ -2,14 +2,34 @@ package sp.lab;
 
 public class Paragraph extends Element {
     private final String text;
+    private AlignStrategy alignStrategy;
 
     public Paragraph(String text) {
         super();
         this.text = text;
+        this.alignStrategy = null; // Default: no alignment strategy
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setAlignStrategy(AlignStrategy alignStrategy) {
+        this.alignStrategy = alignStrategy;
+    }
+
+    public AlignStrategy getAlignStrategy() {
+        return alignStrategy;
     }
 
     @Override
     public void print() {
-        System.out.println("Paragraph: " + text);
+        if (alignStrategy != null) {
+            // Use strategy with default context
+            alignStrategy.render(this, new Context());
+        } else {
+            // Default printing without alignment
+            System.out.println("Paragraph: " + text);
+        }
     }
 }
